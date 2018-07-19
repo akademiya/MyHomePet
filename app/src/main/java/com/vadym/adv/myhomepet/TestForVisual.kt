1483789c4a9101578c80833b18a8eeb56f3ca1ea
vadym <akademiya.adw@gmail.com>
2018-07-19 21:00:05 +0300

    card list & card edit

---
+++ app/src/main/java/com/vadym/adv/myhomepet/TestForVisual.kt
@@ -0,0 +1,12 @@
+package com.vadym.adv.myhomepet
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TestForVisual : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.view_my_pet_card_edit)
+        super.onCreate(savedInstanceState)
+    }
+}
