8a2b769d701dfb971c522e52b8025e62cf8c0e34
vadym <akademiya.adw@gmail.com>
2018-08-28 21:37:40 +0300

    spinner list in string

--- app/src/main/java/com/vadym/adv/myhomepet/ui/pet/view/EditPetView.kt
+++ app/src/main/java/com/vadym/adv/myhomepet/ui/pet/view/EditPetView.kt
@@ -3,8 +3,10 @@ package com.vadym.adv.myhomepet.ui.pet.view
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.vadym.adv.myhomepet.*
 import com.vadym.adv.myhomepet.data.SqliteDatabase
 import com.vadym.adv.myhomepet.di.module.GlideApp
@@ -18,43 +20,13 @@ class EditPetView : BaseActivity(), IEditPetView {
     private lateinit var presenter: EditPetPresenter
     private lateinit var database: SqliteDatabase
     private lateinit var petModel: PetModel
+    private val isLocked = SimpleLock()
 //    private val idCard = intent.getIntExtra(ID_CARD, 0)
     private var category = ""
     private var action = ""
     private var period = ""
     private var country = ""
 
-//    lateinit var formatHelper: FormatHelper
-//
-//    private val spinnerMainCategoryAdapter = SpinnerAdapter<PetActionSpinner> { isDropDown, position, convertView, parent ->
-//        val convertView = convertView ?: if (isDropDown) {
-//            LayoutInflater.from(baseContext).inflate(R.layout.spinner_simple_item, parent, false)
-//        } else {
-//            LayoutInflater.from(baseContext).inflate(R.layout.spinner_drop_down, parent, false)
-//        }
-//
-//        convertView.value.apply {
-//            val mainAction = items[position]
-//            text = formatHelper.formatPetAction(mainAction)
-//
-////            if (isDropDown) {
-////                val backgroundColor = if (isItemSelected(diningOption)) filterSelectedBackground else filterNormalBackground
-////                convertView.setBackgroundColor(backgroundColor)
-////            }
-//        }
-//
-//        convertView
-//    }
-
-    private val spinner_category = arrayOf(
-            "категория питомца",
-            "собака", "кошка", "морская свинка",
-            "декоративная мишь", "хомяк", "белка", "шиншила",
-            "попугай", "сова", "канарейка", "курица",
-            "паук", "декоративный таракан", "бабочка"
-    )
-
-    private val spinner_acton = arrayOf("отдам в хорошие руки", "отдам на время")
 
     @SuppressLint("RestrictedApi")
     override fun init(savedInstanceState: Bundle?) {
@@ -98,26 +70,31 @@ class EditPetView : BaseActivity(), IEditPetView {
 //            }
 //        }
 
-        val spinnerCategoryAdapter = ArrayAdapter(this, R.layout.spinner_simple_item, spinner_category)
+        val spinnerCategoryAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.pet_category,
+                R.layout.spinner_simple_item
+        )
         spinnerCategoryAdapter.setDropDownViewResource(R.layout.spinner_drop_down)
         spinner_categories.adapter = spinnerCategoryAdapter
-//        spinner_categories.adapter = spinnerMainActionAdapter
         spinner_categories.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-//                val selectedItem = spinnerMainActionAdapter.getItem(position)
-//                spinnerMainActionAdapter.selectedItem = selectedItem
-                category = spinner_category[position] // if (position > 0)
+                category = spinnerCategoryAdapter.getItem(position).toString()  // if (position > 0)
             }
         }
 
-        val spinnerActionAdapter = ArrayAdapter(this, R.layout.spinner_simple_item, spinner_acton)
+        val spinnerActionAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.pet_action,
+                R.layout.spinner_simple_item
+        )
         spinnerActionAdapter.setDropDownViewResource(R.layout.spinner_drop_down)
         spinner_action.adapter = spinnerActionAdapter
         spinner_action.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {}
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                action = spinner_acton[position]
+                action = spinnerActionAdapter.getItem(position).toString()
             }
         }
 
@@ -136,11 +113,12 @@ class EditPetView : BaseActivity(), IEditPetView {
     }
 
     override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
         presenter.unbindView(this)
+        super.onDetachedFromWindow()
     }
 
     override fun setCreateOrEditTitle(isEdit: Boolean) {
+        if (!isLocked.isLocked) return
         toolbar_title.setText(if (isEdit) R.string.edit_item else R.string.create_item)
         button_delete_me.visibility = isEdit.toAndroidVisibility()
     }
